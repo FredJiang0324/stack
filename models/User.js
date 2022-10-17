39e3030650cbc34ea4d86a7bc470d9bf3d014ca1
fred <dj37@rice.edu>
2022-10-17 16:19:01 -0500

    Second commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.SchemaType({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String,
+    },
+    data:{
+        type: data,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
